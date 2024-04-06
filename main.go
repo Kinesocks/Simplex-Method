866adc8ef8d6ebb75b1959a61f7d3430d9c5403c
Kinesocks <ivangr2002@gmail.com>
2024-04-06 19:05:12 +0300

    Backup for another machines

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func printSystem(A [][]float32, b, c []float32, signs []string) {
 	println()
 }
 
-func preprocess(A [][]float32, b, c []float32, signs []string) {
+func preprocess(A [][]float32, b, c []float32, signs []string) ([][]float32, []float32, []float32) {
 	fmt.Print("\nПриводим систему к каноническому виду\nДля этого умножим на -1 те строки где b < 0\n",
 		"А также добавим дополнительные переменные, чтобы избавиться от неравенств\n",
 		"Исходная система ограничений:\n",
@@ -26,6 +26,7 @@ func preprocess(A [][]float32, b, c []float32, signs []string) {
 	printSystem(A, b, c, signs)
 
 	for i := 0; i < len(b); i++ {
+		// если b оказалось отрицательным числом
 		if b[i] < 0 {
 			b[i] *= -1
 			for j := 0; j < len(A[i]); j++ {
@@ -37,10 +38,23 @@ func preprocess(A [][]float32, b, c []float32, signs []string) {
 				signs[i] = ">="
 			}
 		}
-		A[i] = append(A[i], 1)
+		switch signs[i] {
+		case ">=":
+			A[i] = append(A[i], -1, 1)
+		case "<=":
+			A[i] = append(A[i], 1)
+		default:
+			fmt.Printf("Нет знака неравенства %v", A[i])
+		}
+		signs[i] = "="
 	}
-	println("Система приведенная к каноническому виду")
+	println("Система приведенная к каноническому виду:")
 	printSystem(A, b, c, signs)
+	return A, b, c
+}
+
+func simplex(A [][]float32, b, c []float32) {
+
 }
 
 func main() {
@@ -49,5 +63,6 @@ func main() {
 	c := []float32{2, 1}
 	signs := []string{">=", ">=", "<=", "<="}
 	// printSystem(A, b, c, signs)
-	preprocess(A, b, c, signs)
+	A1, b1, c1 := preprocess(A, b, c, signs)
+	simplex(A1, b1, c1)
 }
